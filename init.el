1cdc253aae7e55ab1bf70607c7ded87aed88bced
Pawel Kucharski <pawel.kucharski@nokia.com>
2019-10-07 19:00:56 +0200

    Spotify, local packages, powerline, htmlize, minor fixes and so on

--- init.el
+++ init.el
@@ -3,9 +3,30 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(hl-todo-keyword-faces
+   (quote
+    (("TODO" . "#dc752f")
+     ("NEXT" . "#dc752f")
+     ("THEM" . "#2d9574")
+     ("PROG" . "#4f97d7")
+     ("OKAY" . "#4f97d7")
+     ("DONT" . "#f2241f")
+     ("FAIL" . "#f2241f")
+     ("DONE" . "#86dc2f")
+     ("NOTE" . "#b1951d")
+     ("KLUDGE" . "#b1951d")
+     ("HACK" . "#b1951d")
+     ("TEMP" . "#b1951d")
+     ("FIXME" . "#dc752f")
+     ("XXX+" . "#dc752f")
+     ("\\?\\?\\?+" . "#dc752f"))))
  '(package-selected-packages
    (quote
-    (magit spacemacs-theme elpy flycheck which-key use-package try org-bullets counsel auto-complete ace-window))))
+    (powerline htmlize magit spacemacs-theme elpy flycheck which-key use-package try org-bullets counsel auto-complete ace-window)))
+ '(pdf-view-midnight-colors (quote ("#b2b2b2" . "#292b2e"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -14,17 +35,18 @@
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
 
 ;; my-stuff
-
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '(("melpa" . "https://melpa.org/packages/")
-	       ("melpa-mirror" . "~/.emacs.d/mirror-elpa/melpa/")))
+;; (add-to-list 'package-archives
+;; '("melpa" . "https://melpa.org/packages/"))
 
+(setq package-check-signature nil)
+(setq package-archives
+      '(("melpa-mirror" . "~/.emacs.d/mirror-elpa/melpa/")
+	("gnu-mirror" . "~/.emacs.d/mirror-elpa/gnu/")))
 
 
 (package-initialize)
-
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
