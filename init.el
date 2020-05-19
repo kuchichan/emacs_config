1256705b7b46445b930f497e994a9778019aaada
kuchichan <pawel.kucharski@hotmail.com>
2020-05-19 21:23:46 +0200

    use package cleanup ms server

--- init.el
+++ init.el
@@ -3,6 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ccls-executable "/snap/bin/ccls")
+ '(ccls-sem-highlight-method 'font-lock)
+ '(company-idle-delay 0.0 t)
+ '(company-minimum-prefix-length 1 t)
  '(custom-safe-themes
    '("3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "c48551a5fb7b9fc019bf3f61ebf14cf7c9cdca79bcb2a4219195371c02268f11" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "174502267725776b47bdd2d220f035cae2c00c818765b138fea376b2cdc15eb6" "669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
  '(global-spotify-remote-mode t)
@@ -22,10 +26,14 @@
      ("FIXME" . "#dc752f")
      ("XXX+" . "#dc752f")
      ("\\?\\?\\?+" . "#dc752f")))
+ '(ivy-display-style 'fancy)
+ '(ivy-use-virtual-buffers t)
+ '(lsp-keymap-prefix "s-l")
  '(nyan-mode t)
  '(package-selected-packages
-   '(yasnippet-classic-snippets yasnippet dap-mode nyan-mode embrace sublime-themes lsp-ui company-lsp go-autocomplete go-mode cider rainbow-delimiters clojure-mode-extra-font-locking clojure-mode paredit pow powerline htmlize magit spacemacs-theme elpy flycheck which-key use-package try org-bullets counsel auto-complete ace-window))
- '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e")))
+   '(smartparens smartparens-config smartparents-config wrap-region lsp-python-ms gcmh yasnippet-classic-snippets yasnippet dap-mode nyan-mode embrace sublime-themes lsp-ui company-lsp go-autocomplete go-mode cider rainbow-delimiters clojure-mode-extra-font-locking clojure-mode paredit pow powerline htmlize magit spacemacs-theme elpy flycheck which-key use-package try org-bullets counsel auto-complete ace-window))
+ '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e"))
+ '(setq "/snap/bin/ccls" t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
